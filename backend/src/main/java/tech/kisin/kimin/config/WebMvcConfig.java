3b308b84aa5a82acd2000353dba65f0f9932dfa1
KisinTheFlame <kisintheflame@gmail.com>
2022-02-28 15:58:27 +0800

    Fix

--- backend/src/main/java/tech/kisin/kimin/config/WebMvcConfig.java
+++ backend/src/main/java/tech/kisin/kimin/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package tech.kisin.kimin.config;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootConfiguration
@@ -12,5 +13,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addMapping("/**")
                 .allowedOriginPatterns("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/avatar/**")
+                .addResourceLocations("file:/kimin/image/avatar/");
     }
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {HomeContainer} from './Home';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <HomeContainer/>
+    );
 }
 
 export default App;
